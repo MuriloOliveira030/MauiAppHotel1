74818c7bbb0d107a7a3fcb8011a6af0eda5676e1
MuriloOliveira030 <muriloandrade030@gmail.com>
2025-06-01 18:57:43 -0300

    Add recursos ao app

--- MauiAppHotel1/App.xaml.cs
+++ MauiAppHotel1/App.xaml.cs
@@ -1,12 +1,41 @@
-﻿namespace MauiAppHotel1
+﻿using MauiAppHotel1.Models;
+
+namespace MauiAppHotel1
 {
     public partial class App : Application
     {
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                Descricao = "Suíte Premium",
+                ValorDiariaAdulto = 120.0,
+                ValorDiariaCrianca = 70.0,
+                },
+            new Quarto()
+            {
+                Descricao = "Suíte Super Luxo",
+                ValorDiariaAdulto = 100.0,
+                ValorDiariaCrianca = 50.0,
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Luxo",
+                ValorDiariaAdulto = 80.0,
+                ValorDiariaCrianca = 50.0,
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Crise",
+                ValorDiariaAdulto = 50.0,
+                ValorDiariaCrianca = 25.0,
+            }
+        };
         public App()
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new Views.NewPage1());
+            MainPage = new NavigationPage(new Views.ContratacaoHospedagem());
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
--- MauiAppHotel1/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel1/Views/ContratacaoHospedagem.xaml.cs
@@ -1,10 +1,53 @@
 namespace MauiAppHotel1.Views;
 
-public partial class NewPage1 : ContentPage
+public partial class ContratacaoHospedagem : ContentPage
 {
-	public NewPage1()
-	{
+    App PropriedadesApp;
+    private object pck_quarto;
+    private object dtpck_checkin;
+    private object dtpck_checkout;
+
+    public object DameTime { get; private set; }
+
+    public ContratacaoHospedagem()
+    {
         InitializeComponent();
 
+        PropriedadesApp = (App)Application.Current;
+
+        pck_quarto.ItemsSource = PropriedadesApp.lista_quartos;
+
+        dtpck_checkin.MinimumDate = DateTime.Now;
+        dtpck_checkin.MaximumDate = new DateTime(DateTime.Now. Year, DateTime.Now.Month + 1, DateTime.Now.Day);
+
+        dtpck_checkout.MinimumDate = dtpck_checkin.Date.AddDays(1);
+        dtpck_checkout.MaximumDate = dtpck_checkin.Date.AddMonths(6);
+
     }
+
+    private void InitializeComponent()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Buttom_Clicked(Object sender, EventArgs e)
+    {
+        try
+        {
+            Navigation.PushAsync(new HospedagemContrada());
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert = ("Ops", ex.Message, "OK");
+        }
+    }
+
+    private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
+    {
+        DatePicker elemento = sender as DatePicker;
+
+        DateTime data_selecionada_checkin = elemento.Date;
+
+        dtpck_checkout.MinimumDate = data_selecionada_checkin.AddDays(1);
+        dtpck_checkout.MaximumDate = data_selecionada_checkin.AddMonths(6);
 }
